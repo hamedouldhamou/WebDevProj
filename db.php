8f77abcfd3edf46f79295b1d5a31adeff9b6a24e
ammanviii <ammanuelamsal@gmail.com>
2018-12-07 16:52:16 -0500

    Amman: Refreshing gitignore

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-//connect to mysql database
-
-$host = "localhost";
-$user = "houldhamou1";
-$pass = "houldhamou1";
-$db = "houldhamou1";
-
-$con = mysqli_connect($host, $user, $pass, $db) or die("Connection Error " . mysqli_error($con));
